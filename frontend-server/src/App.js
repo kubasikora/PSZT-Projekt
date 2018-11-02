d53005fca90a2155f5c017f4168857dae7fcb6d2
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2018-11-02 20:45:31 +0100

    #23 frontend stub

---
+++ frontend-server/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src="./helix.gif" className="App-logo" alt="logo" />
+          <p className="App-text">
+            Podstawy Sztucznej Inteligencji
+          </p>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
