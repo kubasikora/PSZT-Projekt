b3517d509a4bd78565e3ef565a7006e8481a4b1d
kubasikora <J.Sikora@stud.elka.pw.edu.pl>
2019-01-04 22:03:05 +0100

    finished error test

--- algorithm_server/scheduler.sh
+++ algorithm_server/scheduler.sh
@@ -1,8 +1,8 @@
 echo "Running test 1 with arguments A=5 B=50"
 ./statistic.py 5 50
-echo "Running test 1 with arguments A=26 B=29"
+echo "Running test 2 with arguments A=26 B=29"
 ./statistic.py 26 29
-echo "Running test 1 with arguments A=-10 B=70"
+echo "Running test 3 with arguments A=-10 B=70"
 ./statistic.py -10 70
-echo "Running test 1 with arguments A=14 B=100"
+echo "Running test 4 with arguments A=14 B=100"
 ./statistic.py 14 100
